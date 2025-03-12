a4da6be7f439f9d846c6e8239cc8692b3f92ec8c
leann1109 <lean.obregon1@gmail.com>
2025-03-12 10:22:02 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Mortgage Calculator
+Clear All
+
+Mortgage Amount
+
+Mortgage Term
+
+Interest Rate
+
+Mortgage Type
+Repayment
+Interest Only
+
+Calculate Repayments
+
+{/* <!-- Empty results start --> */}
+
+Results shown here
+
+Complete the form and click “calculate repayments” to see what 
+your monthly repayments would be.
+
+{/* <!-- Empty results end --> */}
+
+{/* <!-- Completed results start --> */}
+
+Your results
+
+Your results are shown below based on the information you provided. 
+To adjust the results, edit the form and click “calculate repayments” again.
+
+Your monthly repayments
+
+Total you'll repay over the term
+
+{/* <!-- Completed results end --> */}
+
+<div className="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+  Coded by <a href="#">Lean Obregon</a>.
+</div>
     </div>
   );
 }
